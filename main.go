7b3b2df817f71e4be81a4f7dc9dbb74a30e66d91
amolwank <info.amol@gmail.com>
2025-08-02 10:04:10 +0530

    Added checkbox NSFW

--- main.go
+++ main.go
@@ -45,17 +45,27 @@ func main() {
     rand.Seed(time.Now().UnixNano())
     r := gin.Default()
 
+    // Load HTML template
     r.SetHTMLTemplate(template.Must(template.ParseFiles("templates/index.html")))
 
+    // Redirect / to /web-image
     r.GET("/", func(c *gin.Context) {
         keyword := c.DefaultQuery("search", "nature")
         c.Redirect(http.StatusFound, fmt.Sprintf("/web-image?search=%s&page=1", keyword))
     })
 
+    // /web-image endpoint (HTML or JSON response)
     r.GET("/web-image", func(c *gin.Context) {
         keyword := c.DefaultQuery("search", "nature")
         page := c.DefaultQuery("page", "1")
+        nsfw := c.DefaultQuery("NSFW", "off")
+
         searchURL := fmt.Sprintf("https://wallhere.com/en/wallpapers?q=%s&page=%s", keyword, page)
+        if nsfw == "on" {
+            searchURL += "&NSFW=on"
+        } else {
+            searchURL += "&NSFW=off"
+        }
 
         images, err := getImageURLsFromPage(searchURL)
         if err != nil || len(images) == 0 {
@@ -71,17 +81,21 @@ func main() {
             images = images[:24]
         }
 
-        pageNum, _ := strconv.Atoi(page)
+        // Support AJAX (fetch) request: return JSON if header is present
+        if c.GetHeader("X-Requested-With") == "XMLHttpRequest" {
+            c.JSON(http.StatusOK, images)
+            return
+        }
 
+        // Otherwise, render full HTML
+        pageNum, _ := strconv.Atoi(page)
         c.HTML(http.StatusOK, "index.html", gin.H{
-            "images":  images,
-            "keyword": keyword,
+            "images":   images,
+            "keyword":  keyword,
+            "nsfw":     nsfw,
             "nextPage": pageNum + 1,
         })
     })
 
     r.Run(":8080")
 }
-
-
-
